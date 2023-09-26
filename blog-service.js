37ee2f2d645214de897eb9d7e2b29e7abfd39ab6
Devon <devonconnelly11@gmail.com>
2023-09-25 20:20:25 -0400

    blog-service.js implementation v1

---
+++ blog-service.js
@@ -0,0 +1,73 @@
+const fs = require("fs"); 
+var posts = [];
+var categories = [];
+
+async function initialize() {
+    return new Promise((resolve, reject) => {
+        post();
+        function post() {
+            fs.readFile('./data/posts.json', 'utf8', (err, data) => {
+                if(err) reject('unable to read file');
+            posts = JSON.parse(data);
+            });
+            category();
+        }
+        function category() {
+            fs.readFile('./data/categories.json', 'utf8', (err, data) => {
+                if(err) reject('unable to read file');
+                categories = JSON.parse(data)
+                resolve('Operation Successful');
+            });
+        }
+        
+    });
+}
+
+async function getAllPosts() {
+    return new Promise((resolve, reject) => {
+        if(posts.length != 0) {
+            resolve(posts);
+        }
+        else {
+            reject('no results returned');
+        }
+    });
+}
+
+async function getPublishedPosts () {
+    return new Promise((resolve, reject) => {
+        if(posts.length != 0) {
+            var publishedPosts = [];
+            posts.forEach(post => {
+                if(post.published == true) {
+                    publishedPosts.push(post);
+                }
+                
+            });
+            resolve(publishedPosts);
+        }
+        else {
+            reject('no results returned');
+        }
+    });
+}
+
+async function getCategories() {
+    return new Promise((resolve, reject) => {
+        if(categories.length != 0) {
+            resolve(categories);
+        }
+        else {
+            reject('no results returned');
+        }
+    });
+}
+async function main() {
+await initialize();
+console.log(await getAllPosts());
+console.log(await getPublishedPosts());
+console.log(await getCategories());
+
+}
+
+main();
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const server = require('./blog-service');
 const express = require('express'); // "require" the Express module
 const app = express(); // obtain the "app" object
 const HTTP_PORT = process.env.PORT || 8080; // assign a port
@@ -13,3 +14,16 @@ app.listen(HTTP_PORT, () => console.log(`server listening on: ${HTTP_PORT}`));
   app.get('/about', (req, res) => {
     res.sendFile('/Users/devonconnelly/Documents/web322/web322-app/views/about.html')
   });
+
+  app.get('/blog', (req, res) => {
+
+  });
+
+  app.get('/posts', (req, res) => {
+    
+  });
+
+  app.get('/categories', (req, res) => {
+    
+  });
+
