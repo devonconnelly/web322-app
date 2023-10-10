0b4d9cab42c73543084923b6da296b59afe17b46
Devon <devonconnelly11@gmail.com>
2023-10-10 12:43:32 -0400

    a3 intgrity ready for submission

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 /*********************************************************************************
-*  WEB322 – Assignment 02
+*  WEB322 – Assignment 03
 *  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part *  of this assignment has been copied manually or electronically from any other source 
 *  (including 3rd party web sites) or distributed to other students.
 * 
-*  Name: Devon Connelly Student ID: 105322218 Date: Sep. 27 2023
+*  Name: Devon Connelly Student ID: 105322218 Date: Oct. 10 2023
 *
 *  Online (Cyclic) Link: https://fair-ruby-bullfrog-gown.cyclic.cloud/about
 *
